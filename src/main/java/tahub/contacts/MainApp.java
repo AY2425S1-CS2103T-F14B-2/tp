468a0abda3975376c09dd9e3bda044f624f73ca3
miloaisdino <miloaisdino@gmail.com>
2024-11-11 01:03:18 +0800

    fix: attendance persistence problem #134

--- src/main/java/tahub/contacts/MainApp.java
+++ src/main/java/tahub/contacts/MainApp.java
@@ -52,7 +52,7 @@ public class MainApp extends Application {
 
     @Override
     public void init() throws Exception {
-        logger.info("=============================[ Initializing App ]===========================");
+        logger.info("=============================[ Initializing AddressBook ]===========================");
         super.init();
 
         AppParameters appParameters = AppParameters.parse(getParameters());
